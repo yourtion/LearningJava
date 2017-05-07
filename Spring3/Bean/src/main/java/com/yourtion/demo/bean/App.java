bc7280e5295ecfa285c84e48aadf39e97ea6951b
YourtionGuo <yourtion@gmail.com>
2017-05-07 09:43:34 +0800

    Spring3 - Bean Scopes

--- Spring3/Bean/src/main/java/com/yourtion/demo/bean/App.java
+++ Spring3/Bean/src/main/java/com/yourtion/demo/bean/App.java
@@ -1,6 +1,6 @@
 package com.yourtion.demo.bean;
 
-import com.yourtion.demo.innerBean.Customer;
+import com.yourtion.demo.customerServices.CustomerService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,9 +12,26 @@ public class App {
 
     public static void main( String[] args )
     {
-        context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+        context = new ClassPathXmlApplicationContext(new String[] {"SpringBeans.xml"});
 
-        Customer obj = (Customer) context.getBean("CustomerBean");
-        System.out.println(obj.toString());
+        System.out.println("Default: ");
+
+        CustomerService CSDA = (CustomerService) context.getBean("CustomerServiceDefault");
+        CSDA.setMessage("Message by CSDA");
+        System.out.println("Message : "+ CSDA.getMessage());
+
+        //retrieve it again
+        CustomerService CSDB = (CustomerService) context.getBean("CustomerServiceDefault");
+        System.out.println("Message : "+ CSDB.getMessage());
+
+        System.out.println("Prototype: ");
+
+        CustomerService CSPA = (CustomerService) context.getBean("CustomerServicePrototype");
+        CSPA.setMessage("Message by CSPA");
+        System.out.println("Message : "+ CSPA.getMessage());
+
+        //retrieve it again
+        CustomerService CSPB = (CustomerService) context.getBean("CustomerServicePrototype");
+        System.out.println("Message : "+ CSPB.getMessage());
     }
 }
---
+++ Spring3/Bean/src/main/java/com/yourtion/demo/customerServices/CustomerService.java
@@ -0,0 +1,16 @@
+package com.yourtion.demo.customerServices;
+
+/**
+ * Created by yourtion on 07/05/2017.
+ */
+public class CustomerService {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
