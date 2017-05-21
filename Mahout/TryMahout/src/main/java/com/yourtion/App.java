9d46135092ab6602e39027d233f3020621703bd9
YourtionGuo <yourtion@gmail.com>
2017-05-21 22:28:17 +0800

    Mahout Init TryMahout

---
+++ Mahout/TryMahout/src/main/java/com/yourtion/App.java
@@ -0,0 +1,13 @@
+package com.yourtion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
