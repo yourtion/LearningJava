6f05f75bc499d54e6efab47a7ef8611c8a854c1b
YourtionGuo <yourtion@gmail.com>
2019-08-31 13:41:18 +0800

    Activiti: 部署流程定义文件

--- Activiti/activiti-demo/src/main/java/com/yourtion/demo/activiti/DemoMain.java
+++ Activiti/activiti-demo/src/main/java/com/yourtion/demo/activiti/DemoMain.java
@@ -4,11 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RepositoryService;
-import org.activiti.engine.RuntimeService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.repository.ProcessDefinition;
-import org.activiti.engine.runtime.ProcessInstance;
 
 /**
  * @author yourtion
@@ -25,6 +23,17 @@ public class DemoMain {
         log.info("流程引擎名称【{}】. 版本版本【{}】",name, version);
 
         // 部署流程定义文件
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
+        deploymentBuilder.addClasspathResource("second_approve.bpmn20.xml");
+        Deployment deployment = deploymentBuilder.deploy();
+        String deploymentId = deployment.getId();
+        ProcessDefinition processDefinition = repositoryService
+                .createProcessDefinitionQuery()
+                .deploymentId(deploymentId)
+                .singleResult();
+        log.info("流程定义文件 【{}】， 流程ID 【{}】", processDefinition.getName(), processDefinition.getId());
+
         // 启动运行流程
         // 处理流程任务
         log.info("结束我们的程序");
