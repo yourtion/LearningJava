33d39cfd0f6fe47aa2454c385eb65088f96cc47c
YourtionGuo <yourtion@gmail.com>
2019-09-05 17:43:22 +0800

    Activiti: 用户任务制定人

---
+++ Activiti/activiti-demo/src/test/java/com/yourtion/demo/activiti/UserTaskTest.java
@@ -0,0 +1,57 @@
+package com.yourtion.demo.activiti;
+
+import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNull;
+import static junit.framework.TestCase.assertEquals;
+
+
+@Slf4j
+public class UserTaskTest {
+
+    public static final String KEY = "my-process";
+
+    @Rule
+    public ActivitiRule activitiRule = new ActivitiRule();
+
+    @Test
+    @Deployment(resources = {"user-task.bpmn20.xml"})
+    public void testTimerBoundary() throws InterruptedException {
+        ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey(KEY);
+        TaskService taskService = activitiRule.getTaskService();
+
+        Task task = taskService.createTaskQuery().taskCandidateUser("user1").singleResult();
+        log.info("find by user1 task = {}", task);
+        assertEquals("User Task", task.getName());
+
+        task = taskService.createTaskQuery().taskCandidateUser("user2").singleResult();
+        log.info("find by user2 task = {}", task);
+        assertEquals("User Task", task.getName());
+
+        task = taskService.createTaskQuery().taskCandidateGroup("group1").singleResult();
+        log.info("find by group1 task = {}", task);
+        assertEquals("User Task", task.getName());
+
+
+        taskService.claim(task.getId(), "user2");
+//        taskService.setAssignee(task.getId(), "user2");
+        task = taskService.createTaskQuery().taskCandidateOrAssigned("user1").singleResult();
+        log.info("find by user1 task = {}", task);
+        assertNull(task);
+
+        task = taskService.createTaskQuery().taskCandidateOrAssigned("user2").singleResult();
+        log.info("find by user2 task = {}", task);
+        assertEquals("User Task", task.getName());
+
+        task = taskService.createTaskQuery().taskCandidateGroup("group1").singleResult();
+        log.info("find by group1 task = {}", task);
+        assertNull(task);
+    }
+}
